9b181e6c7791f915f734728151c1aa57777e63ff
alebedeva <ana.lebedeva97@gmail.com>
2018-09-12 17:36:32 +0300

    Spring first steps

--- src/WorkDictionary.java
+++ src/WorkDictionary.java
@@ -1,28 +1,12 @@
-import enums.CharRange;
-import enums.DictionaryFile;
-import repositories.PropertiesDictionary;
-import repositories.Repository;
-import repositories.RepositoryFile;
-import validators.ValidatorCharsAndLength;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class WorkDictionary {
 
     public static void main(String[] args) {
-        DictionaryFile file1 = DictionaryFile.FILE1;
-        DictionaryFile file2 = DictionaryFile.FILE2;
-        file1.setPath("\\dictionaries\\file1.txt");
-        file2.setPath("\\dictionaries\\file2.txt");
 
-        file1.setProperties(new PropertiesDictionary(4,
-                new CharRange[]{CharRange.LATIN_CAPITAL, CharRange.LATIN_LOWERCASE}));
-        file2.setProperties(new PropertiesDictionary(5,
-                new CharRange[]{CharRange.NUMERALS}));
-
-        Repository repository = new RepositoryFile(new DictionaryFile[]{file1, file2});
-
-        DictionaryBehavior dictionary = new Dictionary(repository, new ValidatorCharsAndLength());
-
-        ConsoleApplication application = new ConsoleApplication(dictionary);
+        ApplicationContext cpx = new ClassPathXmlApplicationContext("resources/dictionary-project.xml");
+        ConsoleApplication application = (ConsoleApplication) cpx.getBean("application");
         application.start();
     }
 
--- src/enums/DictionaryFile.java
+++ src/enums/DictionaryFile.java
@@ -4,10 +4,25 @@ import repositories.PropertiesDictionary;
 
 
 public enum DictionaryFile implements DictionarySrtucture{
-    FILE1, FILE2;
+    FILE1(
+            "\\dictionaries\\file1.txt",
+            new PropertiesDictionary(4,
+                    new CharRange[]{CharRange.LATIN_CAPITAL, CharRange.LATIN_LOWERCASE}
+                    )
+    ), FILE2(
+            "\\dictionaries\\file2.txt",
+            new PropertiesDictionary(5,
+                    new CharRange[]{CharRange.NUMERALS}
+                    )
+    );
 
     private String path;
     private PropertiesDictionary properties;
+
+    DictionaryFile(String path, PropertiesDictionary properties) {
+        this.path = path;
+        this.properties = properties;
+    }
 
     public void setPath(String path) {
         this.path = path;
